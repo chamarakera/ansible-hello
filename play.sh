eadbf0e111381226f0b02e50a22484a7b50e94c5
ckeragala <chamarakera@gmail.com>
2023-12-26 10:46:00 +1100

    fix typo

--- play.sh
+++ play.sh
@@ -1,3 +1,3 @@
 # This command runs playbook.yaml against the local machine and passes an extra 
-# variable hello_from_extra_args. Configure ansible investory for remote execution.
+# variable hello_from_extra_args. Configure ansible inventory for remote execution.
 ansible-playbook playbook.yaml -e "hello_from_extra_args='hello, from extra args'"
