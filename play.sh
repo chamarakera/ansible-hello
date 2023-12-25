287a8f61fe46849d5050a83c2b494b3c562161bc
ckeragala <chamarakera@gmail.com>
2023-12-26 10:12:36 +1100

    rename role

--- play.sh
+++ play.sh
@@ -1,3 +1,3 @@
 # The below command is created to run the playbook on your local machine. 
 # Modify it for remote execution.
-ansible-playbook -c local -i 127.0.0.1 playbook_localhost.yaml -e "hello_from_command_args='hello, from comand line args'"
+ansible-playbook -i 127.0.0.1 playbook.yaml -e "hello_from_command_args='hello, from comand line args'"
