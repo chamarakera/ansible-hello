c3db88d8016773a114c24738b412799d6aee8c34
ckeragala <chamarakera@gmail.com>
2023-12-26 10:15:24 +1100

    rename commit

--- play.sh
+++ play.sh
@@ -1,3 +1,3 @@
-# The below command is created to run the playbook on your local machine. 
-# Modify it for remote execution.
+# The below command runs the playbook on your local machine. 
+# Modify it as necessary for remote execution.
 ansible-playbook -i 127.0.0.1 playbook.yaml -e "hello_from_command_args='hello, from comand line args'"
